002e08d5a2f77d89256031666af7634f7cd8f31c
Abiola Sodiq <rasababe01@gmail.com>
2023-07-26 22:16:23 +0100

    first commit

---
+++ src/Details.js
@@ -0,0 +1,142 @@
+// Enter all your detials in this file
+
+// Profile Image
+import profile from "./assets/Profile-photo.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/Age-Calc.jpg";
+import projectImage2 from "./assets/projects/Weather-App.jpg";
+import projectImage3 from "./assets/projects/age-calculato.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Abiola Sodiq",
+  tagline: "Frontend Developer",
+  img: profile,
+  about:`I am a highly motivated and resourceful Frontend Developer based in Lagos, Nigeria, with a strong passion for crafting user-friendly web experiences. My keen attention to details, interpersonal skills, and drive to expand knowledge make me an asset to any team. I am Proficient in JavaScript, HTML5, CSS3, React, Tailwind CSS, Git and GitHub, I possess expertise in responsive web design, cross-browser compatibility, debugging, and testing. Eager to collaborate on innovative projects, I are open to intern positions or junior website developer roles to further enhance my expertise and make a meaningful impact in the digital world.` 
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/abiola-sodiq/",
+  github: "https://www.github.com/Abiola-Sodiq",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Environmental Health Officer",
+    Company: `Bariga Local Council Development Area`,
+    Location: "Lagos",
+    Type: "Internship",
+    Duration: "Feb 2017 - Dec 2017",
+  },
+  {
+    Position: "Customer Service Relation",
+    Company: `Sao Records`,
+    Location: "Lagos",
+    Type: "Full-Time",
+    Duration: "Jan 2018 - May 2019",
+  },
+  {
+    Position: "Frontend Developer",
+    Company: `JamaJama Project`,
+    Location: "Lagos",
+    Type: "Internship",
+    Duration: "June 2023 - Present",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Senator Abiru Innovation LAb",
+    Location: "Lagos",
+    Type: "Full Time",
+    Duration: "Feb 2023 - Present",
+  },
+  {
+    Position: "Higher National Diploma in Environment Health",
+    Company: `Lagos State College of Health Technology`,
+    Location: "Lagos",
+    Type: "Full Time",
+    Duration: "March 2013 - May 2017",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Word Hunter",
+    image: projectImage1,
+    description: `Word Hunter" is an interactive word-guessing game where players attempt to guess a hidden word by inputting letters. With a limited number of guesses, players must use their deduction skills to uncover the word providing an engaging and challenging experience.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://abiola-sodiq.github.io/newHangman/",
+  },
+  {
+    title: "Weather App",
+    image: projectImage2,
+    description: `Weather App is a simple App that Genterate Realtime Weather of Different Locations Accross the World`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://abiola-sodiq.github.io/weatherApp/",
+  
+  },
+  {
+    title: "Age Calculator",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://abiola-sodiq.github.io/Age-Calculator/",
+    githubLink: "https://github.com",
+  },
+//   {
+//     title: "Project title 4",
+//     image: projectImage4,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   }
+
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "rasababe01@gmail.com",
+  phone: "+2348113666138",
+};
